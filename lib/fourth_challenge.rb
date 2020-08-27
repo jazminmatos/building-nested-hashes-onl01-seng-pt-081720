5d28690788b83666ee18939f4e66a7a7a09be3ca
Jazmin Matos <jmatos1993@gmail.com>
2020-08-27 18:30:41 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,14 +6,14 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-        :name
-        :age
-        :attitude
+        :name => "",
+        :age => "",
+        :attitude => ""
         },
         {
-        :name
-        :age
-        :attitude
+        :name => "",
+        :age => "",
+        :attitude => ""
         }
       ]
    }, 
